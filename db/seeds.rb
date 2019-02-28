7f82a7493cf6cac62483100952647ad63c2aceb3
Lucie Monnier <lucie.monnier@hec.edu>
2019-02-28 14:29:06 +0100

    models and controllers ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+require 'open-uri'
+
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data = JSON.parse(open(url).read)
+
+data["drinks"].each do |ingredient_hash|
+  ingredient = Ingredient.new(name: ingredient_hash["strIngredient1"])
+  ingredient.save
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
