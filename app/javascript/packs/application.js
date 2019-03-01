f6fc9e70714e5728d3c1c479fd6ddb33ef3cf41c
Lucie Monnier <lucie.monnier@hec.edu>
2019-03-01 16:02:24 +0100

    show plus joli

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@ import 'bootstrap';
 import 'select2/dist/css/select2.css';
 
 import { initSelect2 } from '../components/init_select2';
-
 initSelect2();
 
 import { loadDynamicBannerText } from '../components/banner';
@@ -11,3 +10,10 @@ loadDynamicBannerText();
 
 import { ingredientsOnHover } from '../components/ingredients_on_hover';
 ingredientsOnHover();
+
+
+
+const ingredients = document.querySelector(".ingredient-list");
+if (ingredients) {
+  ingredientsOnHover();
+}
